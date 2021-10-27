1407c63315a92e8b7bd92651a1bb6a87c7855697
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-10-27 09:23:57 -0400

    Refactored

--- eqArrays.js
+++ eqArrays.js
@@ -11,22 +11,19 @@ const assertEqual = function(actual, expected) {
 
 // Create a function called eqArrays that takes in two arrays as parameters
 const eqArrays = function(array1, array2) {
-  let result = true;
+  
   // Create a loop to go over the arrays
   for (let i = 0; i < array1.length; i++) {
     // Compared elements in both arrays. If they match, result will be true
-    if (array1[i] === array2[i]) {
-      result;
-      // Otherwise, result will be false and we'll return false right away
-    } else if (array1[i] !== array2[i]) {
+    if (array1[i] !== array2[i]) {
       return false;
-     
+      // Otherwise, result will be false and we'll return false right away
     }
-
   }
-  // Returning the last result test ran
-  return result;
+  return true;
 };
+// Returning the last result test ran
+  
 
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
@@ -34,4 +31,4 @@ eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 5, 3]), true);
