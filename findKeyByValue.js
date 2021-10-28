6d080f2f4c6a1b590ed73942860ddf83ea4d3f08
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-10-28 00:56:14 -0400

    Linted

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,3 @@
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual}  === ${expected}`);
@@ -9,28 +8,28 @@ const assertEqual = function(actual, expected) {
 };
 
 
-/* Implement the function findKeyByValue which takes in an object and a value. It should scan the 
-object and return the first key which contains the given value. If no key with that given value is found, 
+/* Implement the function findKeyByValue which takes in an object and a value. It should scan the
+object and return the first key which contains the given value. If no key with that given value is found,
 then it should return undefined */
 
 
-const findKeyByValue = function(object, value){
-  // Loop through object to obtain the keys with Object.keys() function and for...of 
-  for (const key of Object.keys(object)){ 
+const findKeyByValue = function(object, value) {
+  // Loop through object to obtain the keys with Object.keys() function and for...of
+  for (const key of Object.keys(object)) {
     //console.log("A key from loop:", key); -> Verify
-    // Create a condition to see if the object's key is equivalent to the value parameter 
-    if(object[key] === value){ 
-     // console.log("The key that matches this value:", key) -> Verify
-     return key; // Return key
+    // Create a condition to see if the object's key is equivalent to the value parameter
+    if (object[key] === value) {
+      // console.log("The key that matches this value:", key) -> Verify
+      return key; // Return key
     } else {
-     // console.log("This key isn't a match:", undefined); -> Verify
+      // console.log("This key isn't a match:", undefined); -> Verify
     }
   }
   return undefined;
-}
+};
   
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
