a4ebfe02137cacceba1a64b7c2da11c4e7ceff87
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-10-29 22:31:53 -0400

    Corrected

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Passed: ${actual}  === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
+
 };
 assertEqual(22, 22);
 assertEqual(8, 26);
