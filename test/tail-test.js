8f285cc0ca223066ce0e4c05c222cc8d1aaa142b
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-01 18:36:00 -0400

    Updated w/Mocha & Chai

--- test/tail-test.js
+++ test/tail-test.js
@@ -1,14 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // Checking words array length after chopping it. Should return true, it's returning original array.
-assertEqual(words.length, 2); // Assumed it was using tail function, but it's not. It's using const words array returned should return false.
+describe("#tail", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual((result.length), 2);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual((result[0]), 'Lighthouse');
+  });
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual((result[1]), 'Labs');
+  });
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual((words.length), 3);
+  });
+});
+
+
