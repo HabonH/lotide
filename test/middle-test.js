0b72b7cd10d8b3e980e233dc5ba8663e58f7030e
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-01 18:47:52 -0400

    Updated w/ Mocha & Chai

--- test/middle-test.js
+++ test/middle-test.js
@@ -4,6 +4,4 @@ const middle = require('../middle');
 describe("#middle", () => {
   it("should return [4] for [1, 2, 3, 4, 5, 6, 7]", () =>
   assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]))
-
-
 });
