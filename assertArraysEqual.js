7313715ce50620e4323015022e84d3239eb2127f
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-01 16:11:02 -0400

    DRY code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,5 @@
-// Create a function called eqArrays that takes in two arrays as parameters
-const eqArrays = function(array1, array2) {
-  
-  // Create a loop to go over the arrays
-  for (let i = 0; i < array1.length; i++) {
-    // Compared elements in both arrays. If they match, result will be true
-    if (array1[i] !== array2[i]) {
-      return false;
-      // Otherwise, result will be false and we'll return false right away
-    }
-  }
-  return true;
-};
-// Returning the last result test ran
-  
+const eqArrays = require('./eqArrays');
+
 /* Implement assertArraysEqual which will take in two arrays and console.log an appropriate message
 to the console */
 const assertArraysEqual = function(actual, expected) {
@@ -26,4 +13,4 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 
-assertArraysEqual([1, 2,], [1, 2, 3]);
+module.exports = assertArraysEqual;
